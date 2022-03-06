4fa8cc8b22a6e6f2216147b79cd17abb253e16ec
ClaudioSarmento <claudio-s-i@hotmail.com>
2022-03-06 19:41:40 -0300

    Adição de tempo de espera para a troca de Activity

--- appminhaideia/src/main/java/br/com/appminhaideia/MainActivity.java
+++ appminhaideia/src/main/java/br/com/appminhaideia/MainActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
 // AppCompactActivity vem do SKD do Android e contem tudo que é necessário para gerar o layout da tela
 public class MainActivity extends AppCompatActivity {
 
     String TAG = "APP_MINHA_IDEIA";
+
+    int tempoDeEspera = 1000 * 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +26,14 @@ public class MainActivity extends AppCompatActivity {
 
     private void trocarTela() {
 
-        Log.d(TAG, "Mudando a Tela");
-
-        Intent itSplash = new Intent(MainActivity.this, SplashActivity.class);
-        startActivity(itSplash);
-        finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "Esperando o tempo para mudar de Tela...");
+                Intent itSplash = new Intent(MainActivity.this, SplashActivity.class);
+                startActivity(itSplash);
+                finish();
+            }
+        },tempoDeEspera);
     }
 }
